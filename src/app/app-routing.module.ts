527397bf984cb6176c1127f3d107b81384402826
Muhammadtalaat <eng.muhamedtalaat@gmail.com>
2022-03-09 11:54:01 +0200

    User Dashboard v.1.9 : adding lazy loading to dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'users', loadChildren:()=> import('./users/users.module').then(m => m.UsersModule)},
   {path:'products', loadChildren:()=> import('./products/products.module').then(m => m.ProductsModule)},
+  {path:'customer', loadChildren:()=> import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
 ];
 
 @NgModule({
