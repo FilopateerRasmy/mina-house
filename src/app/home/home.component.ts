477fe95d3242ec10ca49098bf554a0ff44eb4aad
nadafathy99 <53651378+nadafathy99@users.noreply.github.com>
2022-03-27 05:19:00 +0200

    featured section, nav sidebar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { IProduct } from 'src/app/shared/products';
+import { ProductsService } from 'src/app/services/products.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CategoryService } from '../services/category.service';
 import { ICategory } from '../shared/category';
@@ -6,37 +8,45 @@ import { ICategory } from '../shared/category';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-categories!:ICategory[];
-responsiveOptions=[
-  {
+  categories!: ICategory[];
+  products!: IProduct[];
+  responsiveOptions = [
+    {
       breakpoint: '1024px',
       numVisible: 3,
-      numScroll: 3
-  },
-  {
+      numScroll: 3,
+    },
+    {
       breakpoint: '768px',
       numVisible: 2,
-      numScroll: 2
-  },
-  {
+      numScroll: 2,
+    },
+    {
       breakpoint: '560px',
       numVisible: 1,
-      numScroll: 1
-  }
-];
-  constructor(private categoryService:CategoryService) {
-
-   }
+      numScroll: 1,
+    },
+  ];
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductsService
+  ) {}
 
   ngOnInit(): void {
     this.categoryService.getAllCategories().subscribe({
-    next: (result)=>{
-      this.categories = result;
-    }
-    })
+      next: (result) => {
+        this.categories = result;
+      },
+    });
+    this.productService.getAllProducts().subscribe({
+      next: (products) => {
+        this.products = products.products.filter(
+          (product) => product.featured == true
+        );
+      },
+    });
   }
-
 }
